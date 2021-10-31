bf45e4a62d70d37d9c95ffc382141fac1d4b9030
George <emurashkin96@gmail.com>
2021-10-31 16:41:08 +0300

    "Lesson 6 Task 1. Anonymous class"

--- lesson6/src/task2/Main.java
+++ lesson6/src/task2/Main.java
@@ -8,17 +8,20 @@ public class Main {
                 System.out.println("Hello!");
             }
         };
+        greeter1.wish();
         Greeter greeter2 = new Greeter() {
             @Override
             public void wish() {
                 System.out.println("Привет!");
             }
         };
+        greeter2.wish();
         Greeter greeter3 = new Greeter() {
             @Override
             public void wish() {
                 System.out.println("Guten Tag!");
             }
         };
+        greeter3.wish();
     }
 }
